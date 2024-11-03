b47ec3ee2d670df1fe4bc739a9f5394de1e15ff7
hoangvuee <22130328@st.hcmuaf.edu.vn>
2024-11-03 14:45:01 +0700

    js giỏ hàng

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+const countElement = document.getElementById('count'); 
+const subtractButton = document.getElementById('substract');
+const addButton = document.querySelector('.add .icon'); 
+const updateCartButton = document.getElementById('updateCart'); 
+
+
+function updateCount(value) {
+    let currentCount = parseInt(countElement.innerText);
+    currentCount += value;
+    if (currentCount < 1) currentCount = 1;
+    countElement.innerText = currentCount;
+}
+
+
+subtractButton.addEventListener('click', () => updateCount(-1));
+addButton.addEventListener('click', () => updateCount(1));
+
+
+function calculateTotalPrice() {
+    const unitPrice = 45000;
+    const count = parseInt(countElement.innerText);
+    return unitPrice * count;
+}
+updateCartButton.addEventListener('click', () => {
+    const totalPrice = calculateTotalPrice();
+    document.querySelector('.tamtinhsanpham .price').innerText = `${totalPrice.toLocaleString()}đ`;
+    document.querySelector('.rowright .p1').innerText = `${totalPrice.toLocaleString()}đ`;
+     document.getElementById("tongtienthanhtoan").innerText = `${totalPrice.toLocaleString()}đ`;
+});
+
