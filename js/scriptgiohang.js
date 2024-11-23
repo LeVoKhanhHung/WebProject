78dfa1f95bf60816e620f0ed1ca1cf0e1f36a6a2
Vũ-EE <22130328@st.hcmuaf.edu.vn>
2024-11-23 18:53:15 +0700

    thêm chức năng lưu dữ liệu sản phẩm để đưa qua giỏ

--- js/scriptgiohang.js
+++ js/scriptgiohang.js
@@ -1,3 +1,6 @@
+let cart = JSON.parse(localStorage.getItem('cart')) || [];
+let lastProduct = cart[cart.length - 1]; 
+
 
 const countElement = document.getElementById('count'); 
 const subtractButton = document.getElementById('substract');
@@ -12,9 +15,9 @@ function updateCount(value) {
 subtractButton.addEventListener('click', () => updateCount(-1));
 addButton.addEventListener('click', () => updateCount(1));
 function calculateTotalPrice() {
-    const unitPrice = 45000;
+    const unitPrice = product.price;
     const count = parseInt(countElement.innerText);
-    return unitPrice * count;
+    return unitPrice * product.quantity;
 }
 updateCartButton.addEventListener('click', () => {
     const totalPrice = calculateTotalPrice();
@@ -25,3 +28,41 @@ updateCartButton.addEventListener('click', () => {
 
 
 
+
+
+
+function updateCart() {
+    
+    if (cart.length === 0) {
+        alert('Giỏ hàng của bạn đang trống!');
+        return;
+    }
+
+    cart.forEach((product, index) => {
+        
+        let productElement = document.querySelectorAll('.productdetails')[index]; 
+
+        if (productElement) {
+           
+            productElement.querySelector('.thongtinsanpham p').innerText = lastProduct.name;
+
+           
+            productElement.querySelector('.masanpham .psanpham').innerText = lastProduct.id;
+
+         
+            productElement.querySelector('.giasanpham .psanpham').innerText = lastProduct.price;
+
+       
+            productElement.querySelector('.countsanpham .sum').innerText = lastProduct.quantity;
+
+            
+            var total = lastProduct.price * lastProduct.quantity;
+            productElement.querySelector('.tamtinhsanpham .price').innerText = parse(total)+ 'đ';
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    updateCart();
+});
