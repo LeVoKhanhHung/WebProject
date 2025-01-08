46b924913c03bc2afaf972f256bdc588f06295fe
Lê Trường Toản <21130208@st.hcmuaf.edu.vn>
2025-01-08 19:47:37 +0700

    thêm logic cho phương thức lọc sp trong list

--- WebLocal/src/main/java/Controller/list_product.java
+++ WebLocal/src/main/java/Controller/list_product.java
@@ -27,6 +27,20 @@ public class list_product extends HttpServlet {
         int page = 1; // Mặc định là trang 1
         int itemsPerPage = 9; // Số sản phẩm hiển thị mỗi trang
 
+	 // danh mục có ten trong danh sách
+        List<String> categories = List.of("NẤM QUÀ TẶNG", "BỘT NẤM ĂN", "NẤM DƯỢC LIỆU", "CHÀ BÔNG NẤM", "NẤM TƯƠI", "NẤM KHÔ", "PHÔI NẤM");
+
+        // số lượng sản phẩm cho theo loại danh mục
+        Map<String, Integer> categoryProductCounts = new HashMap<>();
+        for (String category : categories) {
+            try {
+                int count = serviceProduct.getCategoryProductCounts(category);
+                categoryProductCounts.put(category, count);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
 	// Kiểm tra tham số `page` từ request
         if (req.getParameter("page") != null) {
             page = Integer.parseInt(req.getParameter("page"));
@@ -34,6 +48,18 @@ public class list_product extends HttpServlet {
         } else if (session.getAttribute("currentPage") != null) {
             page = (int) session.getAttribute("currentPage");
         }
+
+	try {
+            // Tổng số sản phẩm
+            int totalProducts = serviceProduct.getTotalProducts();
+            // Tổng số trang
+            int totalPages = (int) Math.ceil((double) totalProducts / itemsPerPage);
+
+            // Kiểm tra tham số lọc từ request
+            String filterType = req.getParameter("filterType");
+            if (filterType == null || filterType.isEmpty()) {
+                filterType = "default"; // Lọc mặc định nếu không có tham số
+            }
             // Lấy danh sách sản phẩm của trang hiện tại
 //            ListProduct item = serviceProduct.getListProductByPage(page, itemsPerPage);
             ListProduct filteredProducts = serviceProduct.getFilteredProducts(filterType, page, itemsPerPage);
