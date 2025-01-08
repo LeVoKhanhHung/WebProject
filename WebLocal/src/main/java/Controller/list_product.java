0348361aa643f76257a1213e782772d7c0b122bf
Lê Trường Toản <21130208@st.hcmuaf.edu.vn>
2025-01-08 16:33:08 +0700

    Toan (#24)
    * thêm chức năng phân trang
    
    * thêm phương thức phân trang cho ServiceProduct
    
    * thông tin chi tiết sản phẩm
    
    * thêm phân trang vào list_product

--- WebLocal/src/main/java/Controller/list_product.java
+++ WebLocal/src/main/java/Controller/list_product.java
@@ -24,8 +24,16 @@ public class list_product extends HttpServlet {
         ServiceProduct serviceProduct = new ServiceProduct();
         HttpSession session = req.getSession(true);
 
-        
+        int page = 1; // Mặc định là trang 1
+        int itemsPerPage = 9; // Số sản phẩm hiển thị mỗi trang
 
+	// Kiểm tra tham số `page` từ request
+        if (req.getParameter("page") != null) {
+            page = Integer.parseInt(req.getParameter("page"));
+            session.setAttribute("currentPage", page); // Lưu vào session
+        } else if (session.getAttribute("currentPage") != null) {
+            page = (int) session.getAttribute("currentPage");
+        }
             // Lấy danh sách sản phẩm của trang hiện tại
 //            ListProduct item = serviceProduct.getListProductByPage(page, itemsPerPage);
             ListProduct filteredProducts = serviceProduct.getFilteredProducts(filterType, page, itemsPerPage);
